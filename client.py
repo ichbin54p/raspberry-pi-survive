23185e482eaf3fbe8bf2f5c12ea4d475138356f1
ichbin54p <54predstone@gmail.com>
2024-08-06 17:00:32 +0200

    added gameplay

--- client.py
+++ client.py
@@ -12,20 +12,27 @@ except ImportError: pass
 class main:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.mode = input("Mode (pygame/sense): ").lower()
+        self.mode = input("Mode (pygame/sense): ").lower() or "pygame"
         self.mac = (20, 190, 50)
         self.oac = (255, 255, 255)
         self.bac = (0, 0, 0)
         self.dac = (120, 20, 20)
+        self.pbc = (30, 30, 100)
         self.rules = {}
         self.run = True
         self.id = 0
 
-        self.fps = 10
+        self.fps = 20
         self.x = 0
         self.y = 0
+        self.opindex = 0
         self.players = []
         self.hazards = []
+        self.oldplayers = []
+        self.point_blocks = []
+
+        self.kdown = False
+        self.moved = False
 
         match self.mode:
             case "pygame":
@@ -44,7 +51,7 @@ class main:
         try:
             self.sock.send(json.dumps(d).encode())
             return True
-        except:
+        except json.JSONDecodeError:
             print(f"Failed to send {d} to server")
             return False
     def handle_conn(self):
@@ -52,42 +59,72 @@ class main:
         while True:
             if not self.send({"op": "update", "d": {"pos": [self.x, self.y]}}):
                 break
+            else:
+                self.x = 0
+                self.y = 0
+                self.moved = False
+
             d = self.recv()
+
             if d:
                 self.players = d['d']['players']
                 self.hazards = d['d']['hazards']
+                self.point_blocks = d['d']['point_blocks']
+
+                sleep(1/self.fps)
             else:
                 exit("There was an error trying trying to recieve data from the server.")
     def pygame_draw(self):
         self.window.fill(self.bac)
-        for hazard in self.hazards:
-            pygame.draw.rect(self.window, self.dac, (hazard['pos'][0]*100, hazard['pos'][1]*100, 100, 100))
+
+        self.opindex = 0
+
         for player in self.players:
             if player['exists']:
                 if player['id'] == self.id:
                     c = self.mac
                 else:
                     c = self.oac
+                try:
+                    if player['points'] != self.oldplayers[self.opindex]['points']:
+                        print(f"{player['username']} Has {player['points']} points")
+                except KeyError:
+                    print("There was a key error whilst comparing scores, it should be fixed in a moment. If it doesn't fix, please restart the server.")
+
                 pygame.draw.rect(self.window, c, (player['pos'][0]*100, player['pos'][1]*100, 100, 100))
+            
+            self.opindex += 1
+        
+        self.oldplayers = self.players
+
+        for pb in self.point_blocks:
+            pygame.draw.rect(self.window, self.pbc, (pb['pos'][0]*100, pb['pos'][1]*100, 100, 100))
+
+        for hazard in self.hazards:
+            pygame.draw.rect(self.window, self.dac, (hazard['pos'][0]*100, hazard['pos'][1]*100, 100, 100))
     def main_pygame(self):
         while self.run:
             self.key = pygame.key.get_pressed()
-            self.pdir = 0
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
+                if event.type == pygame.KEYUP:
+                    self.moved = False
 
-            self.x = 0
-            self.y = 0
-
-            if self.key[pygame.K_UP]:
-                self.y = -1
-            elif self.key[pygame.K_DOWN]:
-                self.y = 1
-            elif self.key[pygame.K_LEFT]:
-                self.x = -1
-            elif self.key[pygame.K_RIGHT]:
-                self.x = 1
+            if not self.moved:
+                if self.key[pygame.K_UP]:
+                    self.y = -1
+                    self.moved = True
+                elif self.key[pygame.K_DOWN]:
+                    self.y = 1
+                    self.moved = True
+                elif self.key[pygame.K_LEFT]:
+                    self.x = -1
+                    self.moved = True
+                elif self.key[pygame.K_RIGHT]:
+                    self.x = 1
+                    self.moved = True
 
             self.pygame_draw()
             pygame.display.update()
@@ -98,31 +135,74 @@ class main:
         print("started listening for events...")
         while True:
             event = self.sense.stick.wait_for_event()
+            
+            if event.action == "pressed":
+                if event.direction == "up":
+                    self.y = -1
+                    self.moved = True
+                elif event.direction == "down":
+                    self.y = 1
+                    self.moved = True
+                elif event.direction == "left":
+                    self.x = -1
+                    self.moved = True
+                elif event.direction == "right":
+                    self.x = 1
+                    self.moved = True
+            else:
+                self.x = 0
+                self.y = 0
     def main_sense(self):
         Thread(target=self.he_sense, daemon=True).start()
-        while True:
-            self.sense.clear(self.bac)
+        try:
+            while True:
+                self.sense.clear(self.bac)
+                self.opindex = 0
+                
+                try:
+                    for hazard in self.hazards:
+                        self.sense.set_pixel(hazard['pos'][0], hazard['pos'][1], self.dac)
+                    for pb in self.point_blocks:
+                        self.sense.set_pixel(pb['pos'][0], pb['pos'][1], self.pbc)
+                    for player in self.players:
+                        if player['exists']:
+                            if player['id'] == self.id:
+                                c = self.mac
+                            else:
+                                c = self.oac
 
-            for hazard in self.hazards:
-                self.sense.set_pixel(hazard['pos'][0], hazard['pos'][1], self.dac)
-            for player in self.players:
-                if player['exists']:
-                    if player['id'] == self.id:
-                        c = self.mac
-                    else:
-                        c = self.oac
-                    self.sense.set_pixel(player['pos'][0], player['pos'][1], c)
-            sleep(1/self.fps)
+                            try:
+                                if player['points'] != self.oldplayers[self.opindex]['points']:
+                                    print(f"{player['username']} Has {player['points']} points")
+                            except KeyError:
+                                print("There was a key error whilst comparing scores, it should be fixed in a moment. If it doesn't fix, please restart the server.", end="\r")
+
+                            self.sense.set_pixel(player['pos'][0], player['pos'][1], c)
+                        self.opindex += 1
+        
+                    self.oldplayers = self.players
+
+                except ValueError:
+                    print("Pixel out of range", end="\r")
+                sleep(1/self.fps)
+        except KeyboardInterrupt:
+            self.sense.clear()
     def main(self):
-        self.sock.connect((input("IP: "), int(input("PORT: "))))
+        self.sock.connect((input("IP: ") or "127.0.0.1", int(input("PORT: ") or "25565")))
+
         if not self.send({"op": "auth", "d": input("Successfully connected\nUsername for auth: ")}):
             exit("There's a problem with the server")
+
         data = self.recv()
 
         if data:
             print(data)
             Thread(target=self.handle_conn, daemon=True).start()
+
             self.id = data['d']['index']
+            self.players = data['d']['players']
+            self.oldplayers = data['d']['players']
+
             if self.mode == "pygame":
                 self.main_pygame()
             else:
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import json
 from threading import Thread
 from time import sleep
+from random import randint
 
 class ServerData:
     class server:
@@ -10,6 +11,7 @@ class ServerData:
         players = []
         start = False
     hazards = []
+    point_blocks = []
 
 class Conn:
     def __init__(self, s: tuple[socket.socket, tuple], id):
@@ -43,11 +45,11 @@ class Conn:
                 if not self.auth:
                     match d['op']:
                         case "auth":
-                            self.data = {"username": d['d'], "pos": [0, 0], "exists": True, "id": self.id}
+                            self.data = {"username": d['d'], "pos": [0, 6], "exists": True, "id": self.id, "points": 0}
                             print(f"{self.id}: {self.data}")
                             ServerData.server.players[self.id] = self.data
 
-                            if not self.send({"op": "data", "d": {"hazards": ServerData.hazards, "players": ServerData.server.players, "index": self.id}}):
+                            if not self.send({"op": "data", "d": {"hazards": ServerData.hazards, "players": ServerData.server.players, "point_blocks": ServerData.point_blocks, "index": self.id}}):
                                 self.run = False
                             self.auth = True
                         case _:
@@ -69,10 +71,8 @@ class Conn:
                             elif ServerData.server.players[self.id]['pos'][1] < 0:
                                 ServerData.server.players[self.id]['pos'][1] = 0
                             
-                            if not self.send({"op": "update", "d": {"hazards": ServerData.hazards, "players": ServerData.server.players}}):
+                            if not self.send({"op": "update", "d": {"hazards": ServerData.hazards, "players": ServerData.server.players, "point_blocks": ServerData.point_blocks}}):
                                     self.run = False
-                    
-                    sleep(1/20)
             else:
                 print(f"Breaking {self.id} {self.data}, failed to recieve.") 
                 break
@@ -91,18 +91,63 @@ class main:
         while True:
             Thread(target=Conn(self.sock.accept(), ServerData.server.index).handle, daemon=True).start()
             ServerData.server.index += 1
+    def handle_spawns(self):
+        input("Press enter at any time to start the game.\n")
+        ServerData.server.start = True
+        print("spawning...")
+        while ServerData.server.start:
+            ServerData.point_blocks.append({"pos": [randint(0, 7), randint(0, 7)]})
+
+            for _ in range(7):
+                ServerData.hazards.append({"pos": [randint(0, 7), 0], "v": [0, 1]})
+                sleep(0.7)
+            sleep(3)
+    def handle_checks(self):
+        print("handling checks...")
+
+        while True:
+            offset = 0
+
+            for i in range(len(ServerData.hazards)):
+                if ServerData.hazards[i-offset]['pos'][1] > 7:
+                    ServerData.hazards.pop(i-offset)
+                    offset += 1
+                else:
+                    for j in range(len(ServerData.server.players)):
+                        if ServerData.server.players[j]['exists']:
+                            if ServerData.hazards[i-offset]['pos'][0] == ServerData.server.players[j]['pos'][0] and ServerData.hazards[i-offset]['pos'][1] == ServerData.server.players[j]['pos'][1]:
+                                ServerData.server.players[j]['points'] -= 1
+                                if ServerData.server.players[j]['points'] < 0:
+                                    ServerData.server.players[j]['points'] = 0
+            
+            offset = 0
+            
+            try:
+                for i in range(len(ServerData.point_blocks)):
+                    for j in range(len(ServerData.server.players)):
+                        if ServerData.server.players[j]['exists']:
+                            if ServerData.point_blocks[i-offset]['pos'][0] == ServerData.server.players[j]['pos'][0] and ServerData.point_blocks[i-offset]['pos'][1] == ServerData.server.players[j]['pos'][1]:
+                                ServerData.server.players[j]['points'] += 1
+                                ServerData.point_blocks.pop(i-offset)
+                                offset += 1
+            except IndexError:
+                print("Index error with point blocks")
     def main(self):
         for _ in range(ServerData.server.max_players): ServerData.server.players.append({"exists": False})
 
-        self.sock.bind((input("IP: ") or "127.0.0.1", 25565))
+        self.sock.bind((input("IP: ") or "127.0.0.1", int(input("PORT: ") or "25565")))
         self.sock.listen(ServerData.server.max_players)
+
         Thread(target=self.listen, daemon=True).start()
+        Thread(target=self.handle_spawns, daemon=True).start()
+        Thread(target=self.handle_checks, daemon=True).start()
 
         try:
             while True:
                 for i in range(len(ServerData.hazards)):
-                    ServerData.hazards[i]['x'] += ServerData.hazards[i]['v'][0]
-                    ServerData.hazards[i]['y'] += ServerData.hazards[i]['v'][1]
+                    ServerData.hazards[i]['pos'][0] += ServerData.hazards[i]['v'][0]
+                    ServerData.hazards[i]['pos'][1] += ServerData.hazards[i]['v'][1]
+
                 sleep(0.7)
         except KeyboardInterrupt: self.sock.close()
 if __name__ == "__main__":
